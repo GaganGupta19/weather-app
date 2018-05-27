1ba9dadb48a3cba3fecaf7b1bd60f1839e5e892c
Gagan <gagan_gupta@live.in>
2018-05-27 14:49:59 +0530

    handled some errors

--- app.js
+++ app.js
@@ -20,8 +20,14 @@ request({
     url: `https://maps.google.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBYCZPuFhWfCsMXpHtf-MK3r1KagFS5v6E`,
     json: true
 }, (error, response, body) => {
-    console.log(body.results[0].formatted_address);
-    console.log(body.results[0].geometry.location.lat);
-    console.log(body.results[0].geometry.location.lng);
+    if (error){
+        console.log('Unable to connect to google servers');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address');
+    }else if(body.status === 'OK'){
+        console.log(body.results[0].formatted_address);
+        console.log(body.results[0].geometry.location.lat);
+        console.log(body.results[0].geometry.location.lng);
+    }
     //console.log(JSON.stringify(body, undefined, 1));
 });
